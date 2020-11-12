870883b31384c10e92ad28844f9c81b053342c27
ahonn <ahonn95@outlook.com>
2020-11-12 21:23:27 +0800

    feat: createObjectStore when trans oldVersion zero

--- src/InDB.ts
+++ src/InDB.ts
@@ -1,6 +1,9 @@
 import EventEmitter from 'events';
 
-import { getObjectStoreName, getObjectStoreOptions } from './model/objectStore/objectStore';
+import {
+  getObjectStoreName,
+  getObjectStoreOptions,
+} from './model/objectStore/objectStore';
 import { getAttributes } from './model/column/attributes';
 import type { Model } from './model/model';
 
@@ -37,27 +40,34 @@ class InDB {
     this.options = { ...options, ...InDB.defaultOptions };
   }
 
-  open() {
+  private createObjectStore(transaction: IDBTransaction, model: Model) {
+    const name = getObjectStoreName(model.constructor);
+    const options = getObjectStoreOptions(model.constructor);
+    const attributes = getAttributes(model);
+
+    const objectStore = transaction.db.createObjectStore(name, options);
+    this.objectStores.set(name, objectStore);
+
+    const attributesName = Object.keys(attributes);
+    for (const attributeName of attributesName) {
+      const { index, indexName, unique } = attributes[attributeName];
+      if (index) {
+        objectStore.createIndex(indexName, attributeName, { unique });
+      }
+    }
+  }
+
+  public open() {
     const { indexedDB } = this.options;
     const req = indexedDB.open(this.dbName);
-    req.onupgradeneeded = (ev: Event) => {
-      const db = (ev.target as IDBOpenDBRequest).result;
-
-      for (const model of this.models) {
-        const name = getObjectStoreName(model.constructor);
-        const options = getObjectStoreOptions(model.constructor);
-        const attributes = getAttributes(model);
-
-        const objectStore = db.createObjectStore(name, options);
-        this.objectStores.set(name, objectStore);
-
-        const attributesName = Object.keys(attributes);
-        for (const attributeName of attributesName) {
-          const { index, indexName, unique } = attributes[attributeName];
-          if (index) {
-            objectStore.createIndex(indexName, attributeName, { unique })
-          }
+    req.onupgradeneeded = (ev: IDBVersionChangeEvent) => {
+      if (ev.oldVersion === 0) {
+        const transaction = req.transaction;
+        for (const model of this.models) {
+          this.createObjectStore(transaction, model);
         }
+      } else {
+        console.log('upgrade');
       }
     };
   }
